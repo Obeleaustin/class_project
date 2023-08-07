06aa71f3c4243974d3ba0333319a981b46263f71
Obele Austin <obeleoyeinfie@gmail.com>
2023-08-07 19:57:49 +0100

    class

---
+++ index.js
@@ -0,0 +1,22 @@
+let courses = document.getElementById('courses')
+
+let option1 = document.createElement('option')
+let option2 = document.createElement('option')
+let option3 = document.createElement('option')
+let option4 = document.createElement('option')
+let option5 = document.createElement('option')
+let option6 = document.createElement('option')
+let option7 = document.createElement('option')
+
+courses.appendChild(option1)
+courses.appendChild(option2)
+courses.appendChild(option3)
+courses.appendChild(option4)
+// courses.appendChild(option5)
+// courses.appendChild(option6)
+// courses.appendChild(option7)
+
+option1.innerHTML = 'Desktop Publishing'
+option2.innerHTML = 'Electronics'
+option3.innerHTML = 'Web Development'
+option4.innerHTML = 'Computer Hardware Repairs and Maintenance'
