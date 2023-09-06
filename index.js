22a3b7c868f28d3b4169a6209b50fb88f42b278f
Obele Austin <obeleoyeinfie@gmail.com>
2023-09-06 21:40:55 +0100

    am almost done

--- index.js
+++ index.js
@@ -1,42 +1,46 @@
 import {courseData} from './new-module.js';
 import{courses, display, form, option1, option2, option3,
      option4, option5, option6, option7} from './module.js'
-import{para, para1, para2, para3, para4, yes, inputYes, breakLine,
-     no, inputNo, par2, Yes, InputOffline, BreakLine, Online,
-      InputOnline} from './module.js';
+import{para, yes, inputYes, breakLine, no, inputNo, par2, Yes, InputOffline, BreakLine, Online, InputOnline, para1, para2, para3, para4} from './module.js';
 
     let course;
     
-courses.appendChild(option1)
-courses.appendChild(option2)
-courses.appendChild(option3)
-courses.appendChild(option4)
-courses.appendChild(option5)
-courses.appendChild(option6)
-courses.appendChild(option7)
+    courses.appendChild(option1)
+    courses.appendChild(option2)
+    courses.appendChild(option3)
+    courses.appendChild(option4)
+    courses.appendChild(option5)
+    courses.appendChild(option6)
+    courses.appendChild(option7)
 
-// function thisFunction(){
     
     
-form.appendChild(para)
-form.appendChild(inputYes)
-form.appendChild(yes)
-form.appendChild(breakLine)
-form.appendChild(inputNo)
-form.appendChild(no)
-form.appendChild(par2)
-form.appendChild(InputOnline)
-form.appendChild(Online)
-form.appendChild(BreakLine)
-form.appendChild(InputOffline)
-form.appendChild(Yes)
-            
-myFunction();
-courses.addEventListener('change', myFunction)
-function myFunction(){
-            
+    myFunction();
+    courses.addEventListener('change', myFunction)
+    function myFunction(){
+        form.innerHTML= "";
+
+    form.appendChild(para)
+    form.appendChild(inputYes)
+    form.appendChild(yes)
+    form.appendChild(breakLine)
+    form.appendChild(inputNo)
+    form.appendChild(no)
+    form.appendChild(par2)
+    form.appendChild(InputOnline)
+    form.appendChild(Online)
+    form.appendChild(BreakLine)
+    form.appendChild(InputOffline)
+    form.appendChild(Yes)
+    
     let courseSelected = courses.value;
-    let opt3 = option3.value;    
+    let opt1 = option1.value;
+    let opt2 = option2.value;
+    let opt3 = option3.value;
+    let opt4 = option4.value;
+    let opt5 = option5.value;
+    let opt6 = option6.value;
+    let opt7 = option7.value;    
             
     if(courseSelected === opt3){
                 
@@ -45,6 +49,7 @@ function myFunction(){
         let react = document.createElement('label')
         react.innerText = 'React'
         let inputReact = document.createElement('input')
+        inputReact.setAttribute("class", "input")
         inputReact.type = 'radio'
         inputReact.name = 'option3'
         
@@ -53,6 +58,7 @@ function myFunction(){
         let angular = document.createElement('label')
         angular.innerText = 'Angular'
         let inputAngular = document.createElement('input')
+        inputAngular.setAttribute("class", "input")
         inputAngular.type = 'radio'
         inputAngular.name = 'option3'
         
@@ -63,53 +69,52 @@ function myFunction(){
         form.appendChild(inputAngular)
         form.appendChild(angular)
     }
-    // let allInput1 = allInput.value;
-    // console.log(allInput1)
-    for(let i = 0; i <courseData.length; i++){
-        if(courseData[i].courseTitle === courseSelected){
-            course = courseData[i];
-            // co.push(course.courseTitle, course.price, course.courseDescription, course.location)
-            console.log(course)
-        }  
-    }
-}
-    console.log(course)
-
-    let actualPrice = course.price
-    para1.innerText = 'Course: ' + course.courseTitle;
-    para2.innerText = 'Price: ' + course.price;
-    para3.innerText = 'Description: ' + course.courseDescription;
-    para4.innerText = 'Location: ' + course.location
     
-    // let thisFunction = () =>{
-        
-        inputYes.onclick, InputOnline.onclick =(event)=>{
-            // if(event.target.checked){
-                display.innerHTML = ""
-                course.price = actualPrice
-                para2.innerText = 'Price: ' + course.price;
-                display.append(para1,para2,para3,para4);
-            // }
-        }
-        inputYes.onclick, InputOffline.onclick = (event) => {
-            
-            display.innerHTML = ""
-            let increasedPrice = Number(actualPrice) + 100000       
-            course.price = increasedPrice
-            para2.innerText = 'Price: ' + course.price;
-            display.append(para1,para2,para3,para4);
-            
-        }
-let myFunction1 =()=>{
-    InputOnline.onclick, inputNo.onclick =()=>{
-
-        display.innerHTML = "";
-        let increasedPrice = Number(actualPrice) + 200000       
-        course.price = increasedPrice;
-        para2.innerText = 'Price: ' + course.price;
-        display.append(para1,para2,para3,para4);
-        console.log("na me")
-        
+    for(let i = 0; i <courseData.length; i++){
+        if(courseData[i].courseTitle === courseSelected) course = courseData[i];
     }
+    
+        para1.innerText = 'Course: ' + course.courseTitle;
+        para2.innerText = 'Price: ' + course.price;
+        para3.innerText = 'Description: ' + course.courseDescription;
+        para4.innerText = 'Location: ' + course.location
+        let actualPrice = course.price
+        let increasePrice = Number(actualPrice) + 5000;
+        let IncreasePrice = Number(actualPrice) + 100000;
+        let addPrice = Number(actualPrice) + 500000
 
+console.log(course.courseTitle)
+
+let checkInputs = [inputYes,inputNo,InputOnline,InputOffline]
+checkInputs.forEach((input) => {
+    
+    input.addEventListener('click',() => {
+        
+        if(inputYes.checked && InputOnline.checked){
+            display.innerHTML = "";
+            para2.innerText = 'Price: ' + actualPrice
+            display.append(para1,para2,para3,para4);
+        }else if(inputYes.checked && InputOffline.checked) {
+            display.innerHTML = "";
+            para2.innerText = 'Price: ' + increasePrice;
+            display.append(para1,para2,para3,para4);
+        }else if(inputNo.checked && InputOnline.checked){
+            display.innerHTML = "you dey mad";
+        }else if(inputNo.checked && InputOffline.checked){
+            display.innerHTML = "";
+            para2.innerText = 'Price: ' + IncreasePrice;
+            display.append(para1,para2,para3,para4);
+        }
+        if(courseSelected === opt3){
+            display.innerHTML = "";
+        }
+    })
+})
 }
+
+// else if(inputYes.checked && InputOnline.checked && inputAngular){
+//     display.innerHTML = "";
+//     para2.innerText = 'Price: ' + addPrice;
+//     display.append(para1,para2,para3,para4);
+
+// }
--- module.js
+++ module.js
@@ -2,7 +2,6 @@
 let courses = document.getElementById('courses')
 let display = document.getElementById('display')
 let form = document.getElementById('form')
-
 let option1 = document.createElement('option')
 let option2 = document.createElement('option')
 let option3 = document.createElement('option')
@@ -28,7 +27,7 @@ para.innerText = 'Do you have a computer';
 let yes = document.createElement('label');
 yes.innerText = 'Yes';
 let inputYes = document.createElement('input');
-// inputYes.setAttribute("id", "input")
+inputYes.setAttribute("class", "input")
 inputYes.type = 'radio';
 inputYes.value = 'Yes';
 inputYes.name = 'option';
@@ -37,9 +36,8 @@ let breakLine = document.createElement('br');
 
 let no = document.createElement('label');
 no.innerText = 'No';
-
 let inputNo = document.createElement('input');
-// inputNo.setAttribute("id", "input")
+inputNo.setAttribute("class", "input")
 inputNo.type = 'radio';
 inputNo.value = 'No';
 inputNo.name = 'option';
@@ -50,7 +48,7 @@ par2.innerText = 'Mode of learning';
 let Yes = document.createElement('label');
 Yes.innerText = 'Offline';
 let InputOffline = document.createElement('input');
-InputOffline.setAttribute("id", "Input");
+inputNo.setAttribute("class", "input")
 InputOffline.type = 'radio';
 InputOffline.name = 'option2';
 
@@ -59,8 +57,10 @@ let BreakLine = document.createElement('br');
 let Online = document.createElement('label');
 Online.innerText = 'Online';
 let InputOnline = document.createElement('input');
+InputOnline.setAttribute("class", "input")
 InputOnline.type = 'radio';
 InputOnline.name = 'option2';
+
 
 let para1 = document.createElement('p')
 let para2 = document.createElement('p')
@@ -68,4 +68,4 @@ let para3 = document.createElement('p')
 let para4 = document.createElement('p')
 
 export{courses, display, form, option1, option2, option3, option4, option5, option6, option7}
-export{para, para1, para2, para3, para4, yes, inputYes, breakLine, no, inputNo, par2, Yes, InputOffline, BreakLine, Online, InputOnline};
+export{para, yes, inputYes, breakLine, no, inputNo, par2, Yes, InputOffline, BreakLine, Online, InputOnline, para1, para2, para3, para4};
